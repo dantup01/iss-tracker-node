5e5bb52a41943260e5831e23cfd500b7b2c5f70e
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-04-11 11:28:59 +0100

    updated

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var app = express();
 
 var http = require('http');
 var server = http.Server(app)
+
+app.use(express.static('iss'));
+
+server.listen(PORT, function() {
+    console.log('Server is running');
+})
+
