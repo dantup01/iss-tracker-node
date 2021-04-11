406204eca42267fb120466c60bdb655e0299a546
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-04-11 10:50:02 +0100

    add default

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let timeText = document.querySelector('.time');
 let speedText = document.querySelector('.speed');
 let altitudeText = document.querySelector('.altitude');
 let visibilityText = document.querySelector('.visibility');
+
+// default map location
+
+let lat = 51.505;
+let long = -0.09;
+let zoomLevel = 8;
