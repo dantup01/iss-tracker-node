8d95b7fc9fed74be53e32efc39d8a4806f6701b9
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-04-11 10:49:34 +0100

    grab body elements in js

---
+++ script.js
@@ -0,0 +1,6 @@
+let latitudeText = document.querySelector('.latitude');
+let longitudeText = document.querySelector('.longitude');
+let timeText = document.querySelector('.time');
+let speedText = document.querySelector('.speed');
+let altitudeText = document.querySelector('.altitude');
+let visibilityText = document.querySelector('.visibility');
